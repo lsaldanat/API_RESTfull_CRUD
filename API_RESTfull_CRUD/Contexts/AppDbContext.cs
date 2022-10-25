ce577e5f22d947ea82378a6c2b01d3c4b8cf518e
Luis Andre Saldaña Tello <20121162F@uni.pe>
2022-10-25 00:59:42 -0500

    Quede en Startup.cs

---
+++ API_RESTfull_CRUD/Contexts/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using API_RESTfull_CRUD.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_RESTfull_CRUD.Contexts
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Producto> Producto { get; set; }
+    }
+}
--- API_RESTfull_CRUD/Entities/Producto.cs
+++ API_RESTfull_CRUD/Entities/Producto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace API_RESTfull_CRUD.Entities
 {
     public class Producto
     {
+        [Key]
+        public string pro_codigo { get; set; }
+        public string pro_nombre { get; set; }
+        public string pro_descripcion { get; set; }
+        public decimal pro_precio { get; set; }
+
     }
 }
